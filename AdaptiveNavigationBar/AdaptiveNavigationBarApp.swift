243d3f5ec1cea4b8c1d220703f131ddeb3898681
Ben Shutt <shuttbenjamin@gmail.com>
2023-10-31 13:00:16 +0700

    Initial Commit

---
+++ AdaptiveNavigationBar/AdaptiveNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveNavigationBarApp.swift
+//  AdaptiveNavigationBar
+//
+//  Created by Ben Shutt on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
