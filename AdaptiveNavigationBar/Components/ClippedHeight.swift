0934c678d81571c9c83d65ce9062bddab1dc3f4f
Ben Shutt <shuttbenjamin@gmail.com>
2023-11-26 17:59:42 +1100

    Update ClippedHeight.swift

--- AdaptiveNavigationBar/Components/ClippedHeight.swift
+++ AdaptiveNavigationBar/Components/ClippedHeight.swift
@@ -13,12 +13,15 @@ struct ClippedHeight: ViewModifier {
     /// The maximum height of the content
     @State private var maxHeight: CGFloat = 0
 
+    /// Alignment of the content when added a an overlay
+    var alignment: Alignment = .top
+
     /// Map the maximum height to the clipped height (scaling)
     var scaledHeight: (CGFloat) -> CGFloat
 
     func body(content: Content) -> some View {
         Color.clear
-            .overlay(alignment: .top) {
+            .overlay(alignment: alignment) {
                 content
                     .fixedSize(horizontal: false, vertical: true)
                     .onSizeChanged { size in
